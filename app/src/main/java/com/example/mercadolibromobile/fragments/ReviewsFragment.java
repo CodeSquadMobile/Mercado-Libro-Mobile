3486b1e68cde71b8048ea29a2edcf6ce76ee0139
Marcelo Luna <ldl_marcelo@hotmail.com>
2024-10-22 14:01:48 -0300

    agrego botón volver a Mis Reseñas

--- app/src/main/java/com/example/mercadolibromobile/fragments/ReviewsFragment.java
+++ app/src/main/java/com/example/mercadolibromobile/fragments/ReviewsFragment.java
@@ -1,6 +1,5 @@
 package com.example.mercadolibromobile.fragments;
 
-
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -9,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button; // Importar Button
 import android.widget.Toast;
 
 import com.example.mercadolibromobile.R;
@@ -40,6 +40,16 @@ public class ReviewsFragment extends Fragment {
 
         // Cargar las reseñas del usuario logueado
         loadUserReviews();
+
+        // Inicializar el botón "Volver" y añadir la lógica para regresar
+        Button backButton = view.findViewById(R.id.button_volver); // Referenciar el botón
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Utilizar el FragmentManager para retroceder al fragmento anterior
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
 
         return view;
     }
